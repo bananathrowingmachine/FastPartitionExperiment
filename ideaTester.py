4688cd245329b3a6a9a83597b58bc2507283a249
bananathrowingmachine <zakalakari@gmail.com>
2025-04-23 12:40:39 -0600

    made the experimenter better

--- ideaTester.py
+++ ideaTester.py
@@ -1,4 +1,5 @@
 """
+Builds theoretical time complexities of my recursive dp table culling method, and the crazy math DP (which is O(nM/2) compared to regular partition which is O(nM)) complexity for reference)
 Results are here: https://docs.google.com/spreadsheets/d/1EVpkLWv8gCQcz2yu1IRJDw23yp9A61_KYahQDJ6xTzY/edit?usp=sharing
 """
 import random
@@ -14,26 +15,45 @@ def dpTableBuilder(index, goal):
         dpTableBuilder(index + 1, goal - inputArr[index])
         dpTableBuilder(index + 1, goal)
     
-def newExperiement(size: int) -> tuple[int, int]:
+def newExperiement(size: int, givenInts: list[int]) -> tuple[int, int]:
     """
-    Create a random set of size, then return it's time complexities for recursive culling and the DP approach
+    Creates a test input for checking operation count of 
+
+    :param size: The amount of random numbers to add to the testInput
+    :param givenInts: Any pre determined ints to add to the testInput
     """
     global dpDict
     dpDict = {}
     global inputArr
     inputArr = []
-    for _ in range(size): # new random set
-        inputArr.append(random.randint(0, 255))
-    if sum(inputArr) % 2 == 1: # make sure it's even
-        inputArr[0] = inputArr[0] + 1
-    dpTableBuilder(0, int(sum(inputArr)/2))
-    return (len(dpDict) * 2, size * sum(inputArr))
+    if len(givenInts) != 0:
+        inputArr.extend(givenInts)
+    for _ in range(size):
+        inputArr.append(random.randint(-32768, 32767))
+    absValueInput = list(map(abs, inputArr))
+    if sum(absValueInput) % 2 == 1: # make sure it's even
+        absValueInput[0] = absValueInput[0] + 1
+    dpTableBuilder(0, sum(absValueInput)/2)
+    return (len(dpDict) * 2, (size * sum(absValueInput))/2)
 
-for size in range(1, 51):
-    cullingData = []
-    dpingData = []
-    for round in range(1, 21):
-        result = newExperiement(size)
+"""
+For complexity experiments
+"""
+print("How many rounds do you want averaged out?")
+rounds = int(input())
+print("What is your desired max n?")
+endProblemSize = int(input())
+print("Do you have any required ints?")
+stringInput = input().split(' ')
+inputList = []
+for value in stringInput:
+    inputList.append(int(value))
+
+for size in range(1, endProblemSize+1):
+    cullingData = [] # For the theoretical operations count of my recursive culling method with the crazy halving math
+    dpingData = [] # For just using dynamic programming with M/2 math
+    for round in range(1, rounds+1):
+        result = newExperiement(size-len(inputList), inputList)
         cullingData.append(result[0])
         dpingData.append(result[1])
-    print(str(sum(cullingData)/20) + ", " + str(sum(dpingData)/20))
+    print(str(sum(cullingData)/rounds) + ", " + str(sum(dpingData)/rounds))
