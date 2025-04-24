d0c6ef8e25f85fd9e888e7e01b63d527d7c10c60
bananathrowingmachine <zakalakari@gmail.com>
2025-04-24 13:15:33 -0600

    finalized mcm

---
+++ complexityExperiment.py
@@ -0,0 +1,5 @@
+from versions.memoizedCrazyMath import memoizedCrazyMath
+
+testingSet = {1, 2, 7, -3, -5, -2}
+iterations = memoizedCrazyMath.testIterations(testingSet)
+print(iterations)
--- versions/memoizedCrazyMath.py
+++ versions/memoizedCrazyMath.py
@@ -1,46 +1,54 @@
-def subsetSum(index, goal) -> int:
-    global iterationCount
-    iterationCount += 1
-    if goal == 0:
-        return 1
-    if index >= len(absoluteList):
-        return 0
-    withResult = 0
-    withoutResult = 0
-    global answerMap
-    if goal >= absoluteList[index]: # Only bother with withResult if it can fit
-        if (index+1, goal-absoluteList[index]) not in answerMap:
-            withResult = subsetSum(index+1, goal-absoluteList[index])
+class memoizedCrazyMath:
+    def __init__(self):
+        """
+        Algorithm setup. Creates the input and output, parses the incoming numbers then runs partition. Nothing in here is part of the algorithm being tested.
+        """
+        self.iterationCount = 0
+        self.answerMap: dict[tuple[int, int], int] = {}
+        self.absoluteList: list[int] = []
+
+    @classmethod
+    def testIterations(cls, inputSet: set[int]) -> int:
+        """
+        Creates a new instance of the problem, runs the input on it, then returns iteration count. Useful for testing.
+        """
+        solver = cls()
+        solver.partition(inputSet)
+        return solver.iterationCount
+
+    def partition(self, inputSet: set[int]) -> bool:
+        """
+        Converts the results of subsetSum into a boolean depending on certain conditions. Converts input into a list of absolute values.
+        """
+        for number in inputSet:
+            self.absoluteList.append(abs(number))
+        if sum(inputSet) == 0 and 0 in inputSet:
+            return True
+        answer = self.subsetSum(0, sum(self.absoluteList)/2)
+        if sum(inputSet) == 0:
+            return answer > 2
+        return answer > 0
+
+    def subsetSum(self, index, goal) -> int:
+        """
+        Recursively solves the subset sum problem with inputs for partition. 
+        """
+        self.iterationCount += 1 # Increments every time subsetSum is called.
+
+        if goal == 0:
+            return 1
+        if index >= len(self.absoluteList):
+            return 0
+        withResult = 0
+        withoutResult = 0
+        if goal >= self.absoluteList[index]:
+            if (index+1, goal-self.absoluteList[index]) not in self.answerMap:
+                withResult = self.subsetSum(index+1, goal-self.absoluteList[index])
+            else:
+                withResult = self.answerMap[(index+1, goal-self.absoluteList[index])]
+        if (index+1, goal) not in self.answerMap:
+            withoutResult = self.subsetSum(index+1, goal)
         else:
-            withResult = answerMap[(index+1, goal-absoluteList[index])]
-    if (index+1, goal) not in answerMap:
-        withoutResult = subsetSum(index+1, goal)
-    else:
-        withoutResult = answerMap[(index+1, goal)]
-    answerMap[(index, goal)] = withResult + withoutResult
-    return withResult + withoutResult
-
-def outerShell() -> bool:
-    """
-    Converts the results of superFastSubsetSum into a boolean and is the official output of the algorithm, which changes depending on different inputs.
-    """
-    for number in inputSet:
-        absoluteList.append(abs(number))
-    if sum(absoluteList) == 0 and 0 in inputSet:
-        return True
-    answer = subsetSum(0, sum(absoluteList)/2)
-    if sum(inputSet) == 0:
-        return answer > 2
-    return answer > 0
-
-iterationCount = 0
-answerMap: dict[tuple[int, int], int] = {}
-absoluteList: list[tuple[int, int]] = []
-inputSet = {}
-inputStrings = input().splitlines()
-for strNum in inputStrings:
-    inputSet.update(int(strNum))
-
-outerShell() # actually test the program
-
-print(iterationCount)
+            withoutResult = self.answerMap[(index+1, goal)]
+        self.answerMap[(index, goal)] = withResult + withoutResult
+        return withResult + withoutResult
