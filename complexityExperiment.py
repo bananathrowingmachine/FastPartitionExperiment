0d047d330af376e1fa14e31d27ae627dc5061a04
bananathrowingmachine <zakalakari@gmail.com>
2025-04-24 18:58:31 -0600

    more experimenter building

--- complexityExperiment.py
+++ complexityExperiment.py
@@ -5,8 +5,8 @@ All sets inputted into the algorithms will be verified to have an even absolute 
 """
 from versions.memoizedCrazy import memoizedCrazy
 from versions.memoizedNormal import memoizedNormal
-from versions.recursiveNormal import recursiveNormal
 from versions.tabulatedNormal import tabulatedNormal
+from versions.recursiveNormal import recursiveNormal
 
 from collections import namedtuple
 
@@ -16,7 +16,6 @@ class complexityExperiment:
     """
     def __init__(self, size: int):
         self.runRecurse = size <= 25
-        self.sumIndex = 0
         self.sumSize = [int for _ in range(21)]
         self.findAbsSumBounds()
 
@@ -24,16 +23,61 @@ class complexityExperiment:
     def testProblemSize(cls, size: int, repeat = 20) -> list[tuple[float, float, float, float]]:
         """
         Runs a new experiment of set size n. Will repeat the test the given amount of times, get the average, append it to the output, then increment bigS until bigS reaches 20.
+        Do note that once n is larger than 25, recursion testing will stop and the final float will always be None.
         """
         experiment = cls(size)
-        outputList = [[tuple[float, float, float, float]] for _ in range(21)]
-        resultsTuple = namedtuple('Results Tuple', ['memoCrazy', 'memoNormal', 'tabNormal', 'recurseNormal'])
-        return outputList
+        return experiment.runAllSizes()
     
-    def findAbsSumBounds():
+    def findAbsSumBounds(self):
         """
-        Finds the smallest and biggest set of integers, names their sizes sumSize[0] and sumSize[20] respectively, then finds the other 19 values incrementally.
-        The goal is that sumSize[0] is the set of ints of size n with the smallest absolute sum possible, and sumSize[20] is the biggest.
-        The other 19 values should represent 5% increments from sumSize[0] to sumSize[20], so sumSize[5] should have a sum close to 25% of sumSize[20] if sumSize[0] was actually 0.
+        Finds the smallest and biggest set of integers, names their sizes sumSize[0] and sumSize[20] respectively, then finds the other 19 values as 5%% increments from sumSize[0] to sumSize[20].
         """
         pass
+
+    def generateRandomSet(self, size: int, bigS: int) -> set[int]:
+        pass
+
+    def runSingleTest(self, currentConds: tuple[int, int, set[int]]) -> tuple[int, int, int, int]:
+        """
+        Runs each algorithm once. Verifies all algorithms returned the same bool, and will record the parameters and which algorithm disagrees if not. Also returns the iteration count of each.
+        """
+        testSet = currentConds[2]
+        memoCrazy = memoizedCrazy.testIterations(testSet)
+        memoNormal = memoizedNormal.testIterations(testSet)
+        tabNormal = tabulatedNormal.testIterations(testSet)
+        xnor = [memoCrazy[1], memoNormal[1], tabNormal[1]]
+        if self.runRecurse:
+            recurseNormal = recursiveNormal.testIterations(testSet)
+            xnor.append(recurseNormal[1])
+
+        if len(set(xnor)) > 1:
+            self.recordDisagreement(xnor, currentConds)
+
+        return (
+            memoCrazy[0],
+            memoNormal[0],
+            tabNormal[0],
+            recurseNormal[0] if self.runRecurse else None
+        )   
+
+    def runSingleSize(self) -> tuple[float, float, float, float]:
+        pass
+
+    def runAllSizes(self) -> list[tuple[float, float, float, float]]:
+        outputList = [[tuple[float, float, float, float]] for _ in range(21)]
+
+    def recordDisagreement(xnor: list[bool], currentConds: tuple[int, int, set[int]]):
+        algoNames = ["Memoized Crazy", "Memoized Normal", "Tabulated Normal", "Recursive Normal"]
+        culprits = []
+        if len(xnor) == 4: # It's hard to really know who's right, so in the case recursive normal is running, it's always right, and otherwise, it's the majority opinion.
+            truth = xnor[3] 
+        else:
+            if xnor[0] == xnor[1]: truth = xnor[2]
+            if xnor[1] == xnor[2]: truth = xnor[0]
+            else: truth = xnor[1]
+        for i in range(len(xnor)):
+            if xnor[i] != truth:
+                culprits.append(algoNames[i])
+        # TODO: Have this write to 2 sheets. One that has the culprits and the first 2 current conds and then then one that has all 3.
+        
+
