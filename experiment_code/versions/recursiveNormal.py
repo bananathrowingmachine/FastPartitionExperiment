eae327179b9c7ac21ed3382bf6cfd85ff3f9e12b
bananathrowingmachine <zakalakari@gmail.com>
2025-04-29 05:16:17 -0600

    added fake data, seperated manager and graph builder

--- experiment_code/versions/recursiveNormal.py
+++ experiment_code/versions/recursiveNormal.py
@@ -6,11 +6,12 @@ class recursiveNormal:
         """
         Algorithm setup. Creates the input and output, parses the incoming numbers then runs partition. Nothing in here is part of the algorithm being tested.
         """
-        pass
+        self.iterationCount = 0
 
     @classmethod
     def testIterations(cls, inputSet: set[int]) -> tuple[int, bool]:
         """
         Creates a new instance of the problem, runs the input on it, then returns iteration count, as well as if the set can or cannot be subdivided.
         """
-        pass
+        solver = cls()
+        return (solver.iterationCount, False)
--- graphBuilder.py
+++ graphBuilder.py
@@ -1,48 +1,15 @@
-from experiment_code.complexityExperiment import complexityExperiment
-from multiprocessing import Process, Queue
-from functools import partial
 import numpy as np
 from typing import Tuple
 
-def collectData(queue: Queue):
-    """
-    Allows data collection to happen in a seperate thread. Takes data and inputs it into the queue.
+class graphBuilder:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def processData(data: Tuple[int, np.ndarray, bool]):
+        """
+        Processes the data into a bunch of neat data tables and graphs.
 
-    :param queue: The data queue.
-    """
-    for n in range(1, 21):
-        size = n * 5
-        queue.put((size, complexityExperiment.testProblemSize, size <= 25))
-
-def activateProcessData(queue: Queue):
-    """
-    Allows data processing to happen in a seperate thread. Takes data inputted into the queue to process.
-
-    :param queue: The data queue.
-    """
-    while keepGoing:
-        try:
-            processData(queue.get(timeout=0.1))
-        except Queue.empty:
-            continue
-
-def processData(data: Tuple[int, np.ndarray, bool]):
-    """
-    Processes the data into a bunch of neat data tables and graphs.
-
-    :param data: The data to process. Includes the problem size tested, and if recursive normal was run. 
-    """
-    pass
-
-"""
-The main method. Starts up the threads, flags, and gets everything moving.
-"""
-queue = Queue()
-keepGoing = True
-collector = Process(target=collectData, args=(queue,))
-processor = Process(target=activateProcessData, args=(queue,))
-collector.start()
-processor.start()
-collector.join()
-keepGoing = False
-processor.join()
+        :param data: The data to process. Includes the problem size tested, and if recursive normal was run. 
+        """
+        pass
