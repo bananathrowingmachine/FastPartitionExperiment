ffbc3127fe0c5a2a9ac897db9d7f37ac362926d9
bananathrowingmachine <zakalakari@gmail.com>
2025-05-13 01:06:01 -0600

    improved the data tables

--- data_processing_code/MainDataProcessor.py
+++ data_processing_code/MainDataProcessor.py
@@ -1,13 +1,14 @@
 """
 Processes all of the data into data tables and graphs. 
 
-Made by bananathrowingmachine and Earthquakeshaker2 on May 9th, 2025.
+Made by bananathrowingmachine and Earthquakeshaker2 on May 13th, 2025.
 """
 from data_processing_code.MiscDataCode import ResultsWrapper
 import numpy as np
 import pandas as pd
 from pathlib import Path
 from tabulate import tabulate
+import xlsxwriter
 
 class MainDataProcessor:
     """
@@ -23,21 +24,16 @@ class MainDataProcessor:
         self.singleAlgGraphsDir = graphsDir / "single_algorithm_graphs"
         self.otherGraphsDir = graphsDir / "other_graphs"
 
-        tablesDir = genFilesDir / "data_tables"
-        self.singleAlgTablesDir = tablesDir / "single_algorithm_tables"
-        self.otherTablesDir = tablesDir / "other_tables"
+        self.tablesDir = genFilesDir / "data_tables"
 
-        for directory in [graphsDir, self.singleAlgGraphsDir, self.otherGraphsDir, tablesDir, self.singleAlgTablesDir, self.otherTablesDir]:
+        for directory in [graphsDir, self.singleAlgGraphsDir, self.otherGraphsDir, self.tablesDir]:
             directory.mkdir(parents=True, exist_ok=True)
 
         self.xValues = [f'x_{i}' for i in range(5, 101, 5)]
         self.yValues = [f'y_{i}' for i in range(21)]
-        self.zNames = ['targetSum', 'memoCrazy', 'memoNormal', 'tabCrazy', 'tabNormal', 'recurseNormal']
+        self.zNames: dict[str, str] = {'memoCrazy': 'Memoized Crazy', 'memoNormal': 'Memoized Normal', 'tabCrazy': 'Tabulated Crazy', 'tabNormal': 'Tabulated Normal', 'recurseNormal': 'Recursive Normal', 'targetSum': 'Absolute Sum Target'}
 
-        self.dataTables: dict[str, pd.DataFrame] = {
-            zName: pd.DataFrame(columns=self.yValues, index=self.xValues, dtype=np.float64)
-            for zName in self.zNames
-        }
+        self.dataTables: dict[str, pd.DataFrame] = {zNameShort: pd.DataFrame(columns=self.yValues, index=self.xValues, dtype=np.float64) for zNameShort in self.zNames.keys()}
         self.dataTables['targetSum'] = pd.DataFrame(columns=self.yValues, index=self.xValues, dtype=np.int64)
 
     def appendData(self, results: ResultsWrapper) -> None:
@@ -46,14 +42,27 @@ class MainDataProcessor:
 
         :param data: The data to get processed, wrapped up with the x index and estimated exponential time value.
         """
-        xIndex = results.IntCount
+        xIndex = f'x_{results.IntCount}'
         rawData = results.RawData   
 
-        for zName in self.zNames:
-            yData = np.array([row[zName] for row in rawData])
-            self.dataTables[zName].loc[xIndex] = yData
+        for zNameShort in self.zNames.keys():
+            if zNameShort == 'recurseNormal' and results.RecurseEstimate is not None:
+                yData = np.array([results.RecurseEstimate for _ in range(21)])
+            else:
+                yData = np.array([row[zNameShort] for row in rawData])
+            self.dataTables[zNameShort].loc[xIndex] = yData
 
-        with pd.ExcelWriter(self.singleAlgTablesDir / "results.xlsx", engine='openpyxl') as writer:
-            for zName, df in self.dataTables.items():
-                df.to_excel(writer, sheet_name=zName)
-        
+        with pd.ExcelWriter(self.tablesDir / "output.xlsx", engine="xlsxwriter") as writer:
+
+            for zNameShort, dataFrame in self.dataTables.items():
+                sheetName = self.zNames[zNameShort]
+                dataFrame.to_excel(writer, sheet_name=sheetName)
+                worksheet = writer.sheets[sheetName]
+                
+                for colIndex, column in enumerate(dataFrame.columns, 0):
+                    maxLength = max((len(f"{x:.15g}") for x in dataFrame[column]), default=0)
+                    maxLength = max(maxLength, len(str(column)))
+
+                    newWidth = min(maxLength + 2, 20)
+                    worksheet.set_column(colIndex, colIndex, newWidth)
+                                
