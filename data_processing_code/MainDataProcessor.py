3a378077523d86c1ad9ef39dc3fd31739ba6c3fe
bananathrowingmachine <zakalakari@gmail.com>
2025-05-13 17:47:07 -0600

    more data table stuff

--- data_processing_code/MainDataProcessor.py
+++ data_processing_code/MainDataProcessor.py
@@ -29,8 +29,8 @@ class MainDataProcessor:
         for directory in [graphsDir, self.singleAlgGraphsDir, self.otherGraphsDir, self.tablesDir]:
             directory.mkdir(parents=True, exist_ok=True)
 
-        self.xValues = [f'x_{i}' for i in range(5, 101, 5)]
-        self.yValues = [f'y_{i}' for i in range(21)]
+        self.xValues = [f'{i}' for i in range(5, 101, 5)]
+        self.yValues = [f'{i}' for i in range(21)]
         self.zNames: dict[str, str] = {'memoCrazy': 'Memoized Crazy', 'memoNormal': 'Memoized Normal', 'tabCrazy': 'Tabulated Crazy', 'tabNormal': 'Tabulated Normal', 'recurseNormal': 'Recursive Normal', 'targetSum': 'Absolute Sum Target'}
 
         self.dataTables: dict[str, pd.DataFrame] = {zNameShort: pd.DataFrame(columns=self.yValues, index=self.xValues, dtype=np.float64) for zNameShort in self.zNames.keys()}
@@ -42,7 +42,7 @@ class MainDataProcessor:
 
         :param data: The data to get processed, wrapped up with the x index and estimated exponential time value.
         """
-        xIndex = f'x_{results.IntCount}'
+        xIndex = f'{results.IntCount}'
         rawData = results.RawData   
 
         for zNameShort in self.zNames.keys():
@@ -52,17 +52,28 @@ class MainDataProcessor:
                 yData = np.array([row[zNameShort] for row in rawData])
             self.dataTables[zNameShort].loc[xIndex] = yData
 
-        with pd.ExcelWriter(self.tablesDir / "output.xlsx", engine="xlsxwriter") as writer:
-
+        with pd.ExcelWriter(self.tablesDir / "results.xlsx", engine="xlsxwriter") as writer:
             for zNameShort, dataFrame in self.dataTables.items():
                 sheetName = self.zNames[zNameShort]
-                dataFrame.to_excel(writer, sheet_name=sheetName)
+                dataFrame.T.to_excel(writer, sheet_name=sheetName)
                 worksheet = writer.sheets[sheetName]
+
+                rowCount, colCount = dataFrame.T.shape
+                emptyRow = rowCount + 1  
+                for column in range(1, colCount + 1):
+                    worksheet.write(emptyRow, column, "")
+                    worksheet.write(emptyRow + 1, column, f'{np.min(rawData[f'{zNameShort}'])}')
+                    worksheet.write(emptyRow + 2, column, f'{np.max(rawData[f'{zNameShort}'])}')
+                    worksheet.write(emptyRow + 3, column, f'{np.mean(rawData[f'{zNameShort}'])}')
                 
-                for colIndex, column in enumerate(dataFrame.columns, 0):
+                worksheet.write(emptyRow + 1, 0, "Minimum:")
+                worksheet.write(emptyRow + 2, 0, "Maximum:")
+                worksheet.write(emptyRow + 3, 0, "Average:")
+                        
+                for colIndex, column in enumerate(dataFrame.columns):
                     maxLength = max((len(f"{x:.15g}") for x in dataFrame[column]), default=0)
                     maxLength = max(maxLength, len(str(column)))
-
-                    newWidth = min(maxLength + 2, 20)
+                    newWidth = min(maxLength, 20) + 2
                     worksheet.set_column(colIndex, colIndex, newWidth)
-                                
+
+                
